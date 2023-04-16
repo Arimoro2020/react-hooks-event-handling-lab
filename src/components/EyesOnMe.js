887b06be42c91ae2c308a2dffc9bbc6fdd0d1554
Adeyemi Arimoro <arimoro71@gmail.com>
2023-04-16 16:10:13 -0500

    Add solutions

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(props) {
+   
+    
+
+
+    return(
+
+        <div>
+            <button type='button' onFocus={() =>console.log('Good!')} 
+            onBlur={() =>console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </div>
+    )
+
+
+}
+
+export default EyesOnMe;
